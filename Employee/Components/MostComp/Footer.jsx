4f2d035c7f88e8d0e65bb18ea3ad7f406049a628
Sapana Dhami <a52986@alunos.ipb.pt>
2024-05-05 19:54:47 +0100

    Employee add

--- Employee/Components/MostComp/Footer.jsx
+++ Employee/Components/MostComp/Footer.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet , TouchableOpacity} from 'react-native';
 import { Octicons } from '@expo/vector-icons';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { MaterialIcons } from '@expo/vector-icons';
 
 export default function Footer() {
+  const handleHomePress = () => {
+    console.log('Home icon pressed');
+  };
   return (
     <View style={styles.container}>
       <View style={styles.footer}>
-        <Octicons name="person" size={24} color="black" style={styles.icon} />
+      
+        {/*<Octicons name="person" size={24} color="black" style={styles.icon} />*/}
+        <TouchableOpacity onPress={handleHomePress}>
         <MaterialCommunityIcons name="home-variant-outline" size={24} color="black" style={styles.icon} />
-        <MaterialIcons name="favorite" size={24} color="black" style={styles.icon} />
+      {/*<MaterialIcons name="favorite" size={24} color="black" style={styles.icon} />*/}
+      </TouchableOpacity>
       </View>
     </View>
   );
@@ -22,14 +28,14 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     right: 0,
-    zIndex: 60,
+    
   },
   footer: {
     flexDirection: 'row',
     justifyContent: 'space-around',
     alignItems: 'center',
-    backgroundColor: '#e5687f', // Add a background color for the footer
-    paddingVertical: 10, // Add vertical padding to create space between icons and the edge of the footer
+    backgroundColor: '#e0a5a5', // Add a background color for the footer
+    paddingVertical: 15, // Add vertical padding to create space between icons and the edge of the footer
   },
   icon: {
     // Add any additional styling for the icons here if needed
