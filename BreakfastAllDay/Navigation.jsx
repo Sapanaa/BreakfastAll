0b457d04e498b51201cceab5971a3ca97e4c5030
Fathia Olubukunmi Onifade <a57332@alunos.ipb.pt>
2024-06-03 15:43:34 +0100

    authetication

--- BreakfastAllDay/Navigation.jsx
+++ BreakfastAllDay/Navigation.jsx
@@ -5,14 +5,15 @@ import StaffManagement from './Components/Admin/StaffManagement';
 import EditAccount from './Components/Admin/EditAccount';
 import EmployeeEvaluation from './Components/Admin/EmployeeEvaluation';
 import AdminLoading from './Components/Admin/AdminLoading';
+import Login from './Components/Admin/AdminLogin';
 
 const Stack = createStackNavigator();
 
 export default function AppNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="HomePage">
-
+      <Stack.Navigator initialRouteName="Login">
+      <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
       <Stack.Screen name="AdminLoad" component={AdminLoading} options={{ headerShown: false }} />
         <Stack.Screen name="StaffManagement" component={StaffManagement} options={{ headerTitle: '', headerBackVisible: true }}/>
         <Stack.Screen name="EditAccount" component={EditAccount} options={{ headerTitle: '', headerBackVisible: true }}/>
